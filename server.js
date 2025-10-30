bc737a0e5ca482fdc92153be353c6d92ef182aa8
ibrahim akil <brhimakil.1@gmail.com>
2025-10-30 18:46:10 +0200

    Simplify config - disable file tracing completely and externalize firebase

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // ENV
 const OWNER_EMAIL = process.env.OWNER_EMAIL || 'Filmsmolotov@gmail.com';
-const SITE_URL = process.env.SITE_URL || 'https://molotovfilms.vercel.app';
+const SITE_URL = process.env.SITE_URL || 'https://molotovfilms.be';
 
 app.use(cors());
 app.use(express.json());
